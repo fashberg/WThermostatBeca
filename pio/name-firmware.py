86d19352443d8a5ce4bd9a35de3ed0b7c7019731
Folke <folke@ashberg.de>
2022-09-11 10:49:14 +0200

    Fix Name-Firmware

--- pio/name-firmware.py
+++ pio/name-firmware.py
@@ -11,6 +11,7 @@ Import("env", "projenv")
 #print(env.Dump())
 #print(projenv.Dump())
 
+print("name-firmware init")
 config = configparser.ConfigParser()
 config.read("platformio.ini")
 appversion = config.get("common", "appversion")
@@ -18,8 +19,13 @@ appversion = config.get("common", "appversion")
 if os.path.exists('platformio_override.ini'):
     config = configparser.ConfigParser()
     config.read("platformio_override.ini")
-    appversion2 = config.get("common", "appversion")
-    if appversion2: appversion=appversion2
+    try:
+        appversion2 = config.get("common", "appversion")
+        if appversion2: appversion=appversion2
+    except:
+        pass
+
+print("name-firmware to {}".format(appversion))
 
 OUTPUT_DIR = "build_output{}".format(os.path.sep)
 #print("name-firmware.py // appversion: {}".format(appversion))
